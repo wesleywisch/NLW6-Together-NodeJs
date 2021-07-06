aadb94f16312c19e830f76c27cf3bebaea6213be
wesley_wisch <wesley_wisch@hotmail.com>
2021-07-06 10:16:48 -0400

    Tratando algumas exeções

--- Valoriza/src/server.ts
+++ Valoriza/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors";
 import { router } from './routes';
 import './database';
 const app = express();
@@ -8,6 +9,19 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) =>{
+    if(err instanceof Error){
+        return res.status(400).json({
+            error: err.message
+        })
+    };
+
+    return res.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+});
 
 
 
